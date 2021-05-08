87ea33e6a778a12b21e66955e955bba808973f0f
7555G <630r63.7555@gmail.com>
2021-05-08 15:45:58 +0000

    Minor changes

--- src/demo.cpp
+++ src/demo.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 
 int main() {
-    npower_sensors::HallSensor hall_sensor;
-    npower_sensors::Wattmeter wattmeter;
+    auto hall_sensor = make_unique<npower_sensors::HallSensor>();
+    auto wattmeter = make_unique<npower_sensors::Wattmeter>();
 
-    cout << "hall sensor value: " << hall_sensor.readValue() << endl;
-    cout << "bus_voltage_V: " << wattmeter.voltage() << endl;
-    cout << "supply_voltage_V: " << wattmeter.supply_voltage() << endl;
-    cout << "shunt_voltage_mV: " << wattmeter.shunt_voltage() << endl;
-    cout << "current_mA: " << wattmeter.current() << endl;
-    cout << "power_mW: " << wattmeter.power() << endl;
+    cout << "hall sensor value: " << hall_sensor->readValue() << endl;
+    cout << "bus_voltage_V: " << wattmeter->voltage() << endl;
+    cout << "supply_voltage_V: " << wattmeter->supply_voltage() << endl;
+    cout << "shunt_voltage_mV: " << wattmeter->shunt_voltage() << endl;
+    cout << "current_mA: " << wattmeter->current() << endl;
+    cout << "power_mW: " << wattmeter->power() << endl;
 
     return 0;
 }
